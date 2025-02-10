ad4efbd1eea21611566a852db86232a28bcd8468
JoseUrbano21 <168897114+JoseUrbano21@users.noreply.github.com>
2025-02-10 21:17:10 +0100

    update

--- TEMA2-SERVIDOR/ServidorSS.java
+++ TEMA2-SERVIDOR/ServidorSS.java
@@ -10,10 +10,14 @@ public class ServidorSS {
         System.out.println("----------------------------------");
         try {
             ServerSocket servidor = new ServerSocket();
+            // Configuramos en una clase el servidor y el puerto
             InetSocketAddress direccion = new InetSocketAddress("192.168.1.104",2000);
+            // .bind para poner el socket en marcha, que se puede lanzar con un .delay, por ejemplo, a 10 segundos
             servidor.bind(direccion);
             System.out.println("Servidor creado y escuchando .... ");
             System.out.println("Dirección IP: " + direccion.getAddress());
+
+            // ----------------------------------------------------------------------- CONEXIÓN CON EL CLIENTE
             Socket enchufeAlCliente = servidor.accept();
             System.out.println("Comunicación entrante");
             InputStream entrada = enchufeAlCliente.getInputStream();
@@ -21,14 +25,19 @@ public class ServidorSS {
             String texto = "";
 
             while (!texto.trim().equals("FIN")) {
+                /*
+                Al poner byte[100], obligamos a que el mensaje tenga 100 caracteres. Si el mensaje final es solo "Hola", los otros
+                96 caracteres te los rellena con 0. Si se hace un .trim() ya no habría problema, ya que elimina los espacios al
+                principio y al final de la cadena String.
+                 */
                 byte[] mensaje = new byte[100];
                 entrada.read(mensaje);
-                texto = new String(mensaje);  //.trim();
+                texto = new String(mensaje).trim();  //.trim();
                 if (texto.trim().equals("FIN")) {
                     salida.write("Hasta pronto, gracias por establecer conexión".getBytes());
                 } else {
                     System.out.println("Cliente dice: " + texto);
-                    salida.write(("Tu mensaje tiene " + texto.trim().length() + " caracteres").getBytes());  //texto.length()
+                    salida.write(("Tu mensaje tiene " + texto.length() + " caracteres").getBytes());  //texto.length()
                 }
             }
 
@@ -37,6 +46,7 @@ public class ServidorSS {
             enchufeAlCliente.close();
             servidor.close();
             System.out.println("Comunicación cerrada");
+            // ------------------------------------------------------------ SE CIERRA LA COMUNICACIÓN CON EL CLIENTE
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
